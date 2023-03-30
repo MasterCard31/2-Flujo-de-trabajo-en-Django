3539b0d2288d5afa40142ec053bf4fccb1708c28
MS-Carlos-02 <sir_31@outlook.es>
2023-03-29 19:51:40 -0500

    init commit

---
+++ encuesta/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'titulo': 'Formulario',
+    }
+    return render(request, 'encuesta/formulario.html', context)
+
+def enviar(request):
+    context = {
+        'titulo': 'Respuesta',
+        'nombre': request.POST['nombre'],
+        'clave': request.POST['password'],
+        'educacion': request.POST['educacion'],
+        'nacionalidad': request.POST['nacionalidad'],
+        'idiomas': request.POST.getlist('idiomas'),
+        'email': request.POST['email'],
+        'website': request.POST['sitioweb'],
+    }
+    return render(request, 'encuesta/respuesta.html', context)
+
+def operaciones(request):
+    context = {
+        'titulo': 'Operaciones básicas',t 
+    }
+    return render(request, 'operaciones.html', context)
